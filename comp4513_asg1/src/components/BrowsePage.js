0500f4345dd25c2575dae9269cb79d3a67825e9f
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2021-11-06 12:15:08 -0600

    Created Header Structure
    Added the links to the header and added header to the different page components.

--- comp4513_asg1/src/components/BrowsePage.js
+++ comp4513_asg1/src/components/BrowsePage.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Favorites from './Favorites.js';
 import Filters from './Filters.js';
 import Matches from './Matches.js';
+import Header from './Header.js';
 
 function BrowsePage(props) {
   return (
     <div>
-      <header>
-        <h1>Browse</h1>
-      </header>
+      <Header/>
       <Favorites addFavorite={props.addFavorite}
                  removeFavorite={props.removeFavorite}
                  favorites={props.favorites}
