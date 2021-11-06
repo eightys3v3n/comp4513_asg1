25e7f3d44b1ea03dfd2024961bbd603d86004106
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2021-11-06 11:36:19 -0600

    Fixed Router issue
    Changed 'Router' to 'Route'.

--- comp4513_asg1/src/App.js
+++ comp4513_asg1/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { Router } from 'react-router-dom';
+import { Route } from 'react-router';
 import HomePage from './components/HomePage.js';
 import BrowsePage from './components/BrowsePage.js';
 import DetailsPage from './components/DetailsPage.js';
 import FavoritesProvider from './components/FavoritesContextProvider.js';
 
-const App = (props) => {  
+const App = (props) => {
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <div>
-      <Router path="/" exact component={HomePage} />
+      <Route path="/" exact component={HomePage} />
       <FavoritesProvider>
         <BrowsePage />
         <DetailsPage />
