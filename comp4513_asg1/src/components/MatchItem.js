221fed7030e78735f0a111ef21c6aa6e5a418941
eightys3v3n <eightys3v3n@gmail.com>
2021-11-06 17:03:05 -0600

    made match items display in table

--- comp4513_asg1/src/components/MatchItem.js
+++ comp4513_asg1/src/components/MatchItem.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 function MatchItem(props) {
   return (
-    <p>{props.play.title}</p>
+    <tr>
+      <td>{props.play.title}</td>
+      <td>{props.play.genre}</td>
+      <td>{props.play.likelyDate}</td>
+    </tr>
   );
 }
 
 export default MatchItem;
+
+
+
+/*
+{
+     "id": "alls_well_that_ends_well", 
+     "filename": "",
+     "title": "All's Well That Ends Well",
+     "likelyDate": "1600",
+	 "genre" : "comedy",
+     "wiki": "https://en.wikipedia.org/wiki/All%27s_Well_That_Ends_Well",
+     "gutenberg": "https://gutenberg.org/ebooks/1529",
+	 "shakespeareOrg" : "https://www.shakespeare.org.uk/explore-shakespeare/shakespedia/shakespeares-plays/alls-well-ends-well/",
+	 "synopsis": "Helen saves the King's life, he gives her his son to marry, who runs away from her, and she tricks him into impregnating her. Everything ends happily.",
+      "desc" : "Bertram is compelled to marry Helena. Bertram refuses to consummate their marriage. He goes to Italy. In Italy he courts Diana. Helena meets Diana. They perform the bed trick. The play is considered one of Shakespeare's problem plays, a play that poses complex ethical dilemmas that require more than typically simple solutions."
+
+   }, 
+*/
