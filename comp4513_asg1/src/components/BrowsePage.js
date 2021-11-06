b9dde4b242416480d250f3c476108e151bf3011f
PedroJanikian <pjani371@mtroyal.ca>
2021-11-06 17:22:38 -0600

    Added grid to browse page (#42)
    Co-authored-by: MarkleSparkle <markfrezelljr@yahoo.ca>

--- comp4513_asg1/src/components/BrowsePage.js
+++ comp4513_asg1/src/components/BrowsePage.js
@@ -8,12 +8,22 @@ function BrowsePage(props) {
   return (
     <div>
       <Header/>
-      <Favorites addFavorite={props.addFavorite}
-                 removeFavorite={props.removeFavorite}
-                 favorites={props.favorites}
-      />
-      <Filters />
-      <Matches />
+      <header>
+        <h1>Browse</h1>
+      </header>
+      <div class="pure-g">
+        <div class="pure-u-1-5">
+          <Favorites addFavorite={props.addFavorite}
+                removeFavorite={props.removeFavorite}
+                favorites={props.favorites}/>
+        </div>
+        <div class="pure-u-2-5">
+          <Filters />
+        </div>
+        <div class="pure-u-2-5">
+          <Matches />
+        </div>
+      </div>
     </div>
   );
 }
