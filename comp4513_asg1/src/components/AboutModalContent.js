e07ebd9cdb288c2f6b695a44b684aea5fc894472
PedroJanikian <pjani371@mtroyal.ca>
2021-11-07 14:06:17 -0700

    Header issue Merge to Master (#67)
    * Updating Header-Issue
    
    Installed the react-modal component.
    
    * Completed About Modal
    
    Added the modal 'about' component. Also added css styling to make the modal and the header a little nicer.
    
    * Updating Header-Issue with the Master branch (#66)
    
    * Added sorting to Matches Table
    
    * Added CSS to table sorting buttons
    
    * Working tabs component
    
    * Fixed basic structure in DetailsPage
    
    Co-authored-by: eightys3v3n <eightys3v3n@gmail.com>
    Co-authored-by: MarkleSparkle <markfrezelljr@yahoo.ca>
    
    * Fixed CSS syntax
    
    Co-authored-by: eightys3v3n <eightys3v3n@gmail.com>
    Co-authored-by: MarkleSparkle <markfrezelljr@yahoo.ca>

---
+++ comp4513_asg1/src/components/AboutModalContent.js
@@ -0,0 +1,14 @@
+
+function AboutModalContent(props) {
+    return (
+        <section className="AboutModalContent">
+            <h1>About Page</h1>
+            <p>Content here</p>
+            <p>Content and here</p>
+            <p>Content and also here</p>
+            <p>Regards.</p>
+        </section>
+    )
+}
+
+export default AboutModalContent;
