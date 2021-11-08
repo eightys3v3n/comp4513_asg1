1242b71027d4a433ee447cffdc1600a9a846ed81
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2021-11-07 17:03:45 -0700

    Passing Play's Name into Play Title

--- comp4513_asg1/src/components/AboutModalContent.js
+++ comp4513_asg1/src/components/AboutModalContent.js
@@ -1,3 +1,4 @@
+import React from "react";
 
 function AboutModalContent(props) {
     return (
--- comp4513_asg1/src/components/PlayTitle.js
+++ comp4513_asg1/src/components/PlayTitle.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PlaysContext from './PlaysContextProvider.js';
+import {useState} from "react";
 
 function PlayTitle(props) {
   const {plays, getPlayByID, getGenres} = PlaysContext(PlaysContext).props.value;
+  const [playName] = useState("");
 
   return (
     <section id='Filter-Section' className="padding">
-      <h2>Play Title</h2>
+      <h2>Play Title: {props.playName}</h2>
     </section>);
 }
 
