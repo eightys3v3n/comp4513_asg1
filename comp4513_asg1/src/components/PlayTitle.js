5da9e066912adcee5ccc30890bf75989334bdbee
PedroJanikian <43455987+PedroJanikian@users.noreply.github.com>
2021-11-07 19:38:39 -0700

    Completing the Play Title
    Completed the Play Title and the content within other than the Text's play title changes (though there is a placeholder for the Text changing the data within).

--- comp4513_asg1/src/components/PlayTitle.js
+++ comp4513_asg1/src/components/PlayTitle.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import PlaysContext from './PlaysContextProvider.js';
+import React, {useContext} from 'react';
 import {useState} from "react";
+import {PlaysContext} from './PlaysContextProvider.js';
+import { useEffect } from 'react';
+import FavoriteIcon from './FavoriteIcon.js';
+import { Link } from 'react-router-dom';
 
 function PlayTitle(props) {
-  const {plays, getPlayByID, getGenres} = PlaysContext(PlaysContext).props.value;
-  const [playName] = useState("");
+  const [playTitle, setPlayTitle] = useState("");
+  const [playSynopsis, setPlaySynopsis] = useState("");
+  const [selected, setSelected] = useState(null);
+  const plays = useContext(PlaysContext);
+  const playObj = plays.getByID(props.playID);
+  
+  useEffect(() => { 
+    if (playObj !== null) {
+      setPlayTitle(playObj.title);
+      setPlaySynopsis(playObj.synopsis);
+    }
+  }, [plays]);
 
+  let playTitleContent;
+  if (props.selected !== "Tab3") {
+    playTitleContent = (
+      <div>{playSynopsis}</div>
+    )
+  } else {
+    playTitleContent = (
+      <div>Placeholder for other stuff</div>
+    )
+  }
+  
   return (
     <section id='Filter-Section' className="padding">
-      <h2>Play Title: {props.playName}</h2>
+      <h2>{playTitle}</h2>
+      {playTitleContent}
+      <Link to="/BrowsePage" className="margin"><button className='pure-button'>Close</button></Link>
+      <FavoriteIcon id={props.playID} />
     </section>);
 }
 
