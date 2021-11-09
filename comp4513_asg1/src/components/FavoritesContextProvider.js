2c3fab21c3615a9cb2cf0dbd1eca49d232ab6c48
MarkleSparkle <markfrezelljr@yahoo.ca>
2021-11-08 22:39:50 -0700

    Added favorites to local storage

--- comp4513_asg1/src/components/FavoritesContextProvider.js
+++ comp4513_asg1/src/components/FavoritesContextProvider.js
@@ -1,15 +1,37 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 
 
 export const FavoritesContext = createContext();
 
 function FavoritesProvider(props) {
   const [favorites, setFavorites] = useState([]);
+  const LOCAL_STORAGE_KEY = 'favs';
+ 
+  useEffect(() => {
+    let localFavorites = localStorage.getItem(LOCAL_STORAGE_KEY);
+    
+    if (localFavorites != null) {
+      let favs = JSON.parse(localFavorites);
+      if (favs.length > 0) {
+        console.log(`Retrieved ${favs.length} favorites from local storage`);
+        console.log({favs});
+        
+        // let newFavorites = [];
+        // for (const fav of localFavorites) {
+        //   newFavorites = [...localFavorites, fav];
+        // }
+        setFavorites(favs);
+
+      }
+    }
+  }, []);
   
   function addFavorite(id) {
     if (!favorites.includes(id)) {
       let newFavorites = [...favorites, id];
       setFavorites(newFavorites);
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newFavorites));
+
       console.log(`Adding favorite ${id}`);
     } else {
       console.log(`Not adding duplicate favorite: ${id}`);
@@ -21,6 +43,7 @@ function FavoritesProvider(props) {
       let newFavorites = [...favorites];
       if (favorites.includes(id)) {
         delete newFavorites[newFavorites.indexOf(id)];
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newFavorites));
         console.log(`Removing favorite ${id}`);
         setFavorites(newFavorites);
       }
