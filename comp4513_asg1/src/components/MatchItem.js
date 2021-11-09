1ab658c825b78d9dd92ad97767668d4a65b4a0b8
eightys3v3n <eightys3v3n@gmail.com>
2021-11-08 23:06:02 -0700

    added full text available icon

--- comp4513_asg1/src/components/MatchItem.js
+++ comp4513_asg1/src/components/MatchItem.js
@@ -4,12 +4,19 @@ import FavoriteIcon from './FavoriteIcon.js';
 
 function MatchItem(props) {
   let play_link = `DetailsPage/${props.play.id}`;
+  let additionalInfo = "";
+
+  if (props.play.filename !== "") {
+    // this is actually a unicode page of text character, don't delete.
+    additionalInfo = "🖹"; // U+1F5B9
+  }
   
   return (
     <tr>
       <td><Link to={play_link}>{props.play.title}</Link></td>
       <td>{props.play.genre}</td>
       <td>{props.play.likelyDate}</td>
+      <td>{additionalInfo}</td>
       <td><FavoriteIcon id={props.play.id} /></td>
       <td><Link to={`DetailsPage/${props.play.id}`}><button className='pure-button'>View</button></Link></td>
     </tr>
