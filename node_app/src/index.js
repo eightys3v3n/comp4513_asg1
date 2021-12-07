dc239d44648cdc8e766cc65acdb66aa07c02328f
eightys3v3n <eightys3v3n@gmail.com>
2021-12-07 11:25:02 -0700

    working mongodb instance and connection example

--- node_app/src/index.js
+++ node_app/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const userSchema = require('./User.js').userSchema;
 require('dotenv').config();
 
 
-let port = process.env.PORT;
+const NODEJS_PORT = process.env.NODEJS_PORT;
+const MONGODB_CONSTRING = process.env.MONGODB_CONSTRING;
 const server = express();
 
 
@@ -11,6 +14,23 @@ server.get('/', (req, res) => {
 });
 
 
-server.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
+async function print_user() {
+  const User = mongoose.model('User', userSchema);
+  let test = await User.find();
+  console.log(test);
+}
+
+
+async function main() {
+  console.log(`Connecting to MongoDB at ${MONGODB_CONSTRING}...`);
+  await mongoose.connect(MONGODB_CONSTRING);
+
+  print_user();
+  
+  server.listen(NODEJS_PORT, () => {
+    console.log(`Listening on ${NODEJS_PORT}`);
+  });
+}
+
+
+main();
