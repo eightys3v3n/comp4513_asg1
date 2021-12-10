f362927241e4c6bc8592f782d7a4747548f3f7d3
eightys3v3n <eightys3v3n@gmail.com>
2021-12-09 17:02:07 -0700

    created play model and example usage

---
+++ node_app/src/Play.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const playSchema = new mongoose.Schema({
+  id:       String,
+  filename: String,
+  title: String,
+  likelyDate: String,
+  genre: String,
+  wiki: String,
+  gutenberg: String,
+  shakespeareOrg: String,
+  synopsis: String,
+  desc: String,
+  playText: {
+    title: String,
+    short: String,
+    persona: [
+      {
+        position: Number,
+        player: String,
+        desc: String
+      }
+    ],
+    acts: [
+      {
+        name: String,
+        scenes: [
+          {
+            name: String,
+            title: String,
+            stageDirection: String,
+            speeches: [
+              {
+                speaker: String,
+                lines: [String]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
+
+module.exports = {playSchema};
