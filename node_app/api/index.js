20a5a9e44e8cb488f487eac1f09b66da751cb5ca
eightys3v3n <eightys3v3n@gmail.com>
2021-12-12 14:04:50 -0700

    added login/logout apis

--- node_app/api/index.js
+++ node_app/api/index.js
@@ -3,10 +3,18 @@ const { Router } = require('express');
 
 const apiRouter = Router();
 
+const { loginRouter } = require('./login.js');
+apiRouter.use('/login', loginRouter);
+
+const { logoutRouter } = require('./logout.js');
+apiRouter.use('/logout', logoutRouter);
+
 const { listRouter } = require('./list.js');
 apiRouter.use('/list', listRouter);
+
 const { playRouter } = require('./play.js');
 apiRouter.use('/play', playRouter);
+
 const { userRouter } = require('./user.js');
 apiRouter.use('/user', userRouter);
 
